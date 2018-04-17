78fd4b3148b5316efa5500fcbaa68505f785ebb8
lgw302 <18530809240@163.com>
2018-04-17 19:56:54 +0800

    调用微信提供的位置接口

--- utils/router.js
+++ utils/router.js
@@ -23,8 +23,8 @@ router.get('/getConfig', function(req, res) {
 
 //获取账户信息
 router.get('/getAccountInfo', function(req, res) {
-    var account = JSON.parse(accountInfo)
-    res.json(account);
+    //var account = JSON.parse(accountInfo);
+    res.json(accountInfo);
 });
 
 //增加中心
